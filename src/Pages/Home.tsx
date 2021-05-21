645968832598e342cee3c0f890ede89d8d43ae27
MaxJGruber <maxjgruber@gmail.com>
2021-05-21 12:11:31 +0200

    heroku deploy

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -43,7 +43,6 @@ export default function Home() {
       <main>
         <HeroSection />
 
-
         {/* Alternating Feature Sections */}
         <Introduction />
 
@@ -106,7 +105,7 @@ export default function Home() {
         <div className="bg-gray-100">
           <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
             <p className="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
-              {changeLanguage().ProfessionalAdvice.title}
+              Here's who we work with
             </p>
             <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
               <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
